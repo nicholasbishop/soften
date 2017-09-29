88b485f0dda0f68949c4ae89cb224d80cedc113b
Nicholas Bishop <nicholasbishop@gmail.com>
2017-09-29 18:50:18 -0400

    Checkpoint

---
+++ soften/app.py
@@ -0,0 +1,46 @@
+import os
+
+import able
+import attr
+
+def generate_setup_py():
+    yield 'from setuptools import setup'
+
+
+def write_file(path, content):
+    with open(path, 'w') as wfile:
+        wfile.write(content)
+        if not content.endswith('\n'):
+            wfile.write('\n')
+
+
+def find_config():
+    # TODO, for now just assume cwd
+    return '.soften.able'
+
+
+@attr.s
+class Config(object):
+    name = attr.ib()
+    version = attr.ib()
+
+    @classmethod
+    def parse(cls, string):
+        parsed = able.parse(string)
+        return cls(name=parsed['name'],
+                   version=parsed['version'])
+
+    @classmethod
+    def load(cls, path):
+        with open(path) as rfile:
+            return cls.parse(rfile.read())
+
+
+def main():
+    config = Config.load(find_config())
+    # TODO
+    # repo_path = ''
+
+    # path_setup_py = os.path.join(repo_path, 'setup.py')
+    # write_file(path_setup_py, generate_setup_py(repo_path))
+    pass
