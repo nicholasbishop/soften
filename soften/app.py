b937169c7d42351679a4bd0b3d14b4fd14a17c25
Nicholas Bishop <nicholasbishop@gmail.com>
2017-10-01 03:25:02 -0400

    Add requirements

--- soften/app.py
+++ soften/app.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import able
@@ -22,12 +23,18 @@ def find_config():
 class Config(object):
     name = attr.ib()
     version = attr.ib()
+    deps = attr.ib()
 
     @classmethod
     def parse(cls, string):
         parsed = able.parse(string)
+        deps = []
+        # TODO make this prettier somehow
+        for dep in parsed['deps']:
+            deps.append(Requirement(dep[0], dep[1]))
         return cls(name=parsed['name'],
-                   version=parsed['version'])
+                   version=parsed['version'],
+                   deps=deps)
 
     @classmethod
     def load(cls, path):
@@ -35,7 +42,29 @@ class Config(object):
             return cls.parse(rfile.read())
 
 
+def parse_cli_args():
+    parser = argparse.ArgumentParser(prog='soften', description='simplify python packaging')
+    parser.add_argument('command', nargs='?', choices=('bump',))
+    return parser.parse_args()
+
+
+@attr.s
+class Requirement(object):
+    name = attr.ib()
+    version = attr.ib()
+    operator = attr.ib(default='~=')
+
+    def __str__(self):
+        return ''.join((self.name, self.operator, self.version))
+
+    @staticmethod
+    def format_list(lst):
+        return '\n'.join((str(item) for item in lst))
+
+
 def main():
+    cli_args = parse_cli_args()
+
     config = Config.load(find_config())
 
     module = codegen.Module([codegen.Import('setuptools'),
@@ -49,3 +78,6 @@ def main():
 
     path_setup_py = os.path.join(repo_path, 'setup.py')
     write_file(path_setup_py, str(module))
+
+    path_requirements_txt = os.path.join(repo_path, 'requirements.txt')
+    write_file(path_requirements_txt, Requirement.format_list(config.deps))
